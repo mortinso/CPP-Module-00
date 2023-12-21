a28583787d6ebeabc315be0f515f4c9074422f64
WudDoo <89762267+WudDoo@users.noreply.github.com>
2023-12-21 16:54:09 +0000

    Start of Contact class

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -1,4 +1,17 @@
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
+# include <iostream>
+
+class Contact {
+	public:
+		Contact();
+	private:
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		int PhoneNumber;
+		std::string DarkestSecret;
+};
+
 #endif
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,4 +1,11 @@
 #ifndef PHONEBOOK_HPP
 # define CONTACT_HPP
 
+# include <iostream>
+# include "Contact.hpp"
+
+class PhoneBook: public Contact {
+
+};
+
 #endif
